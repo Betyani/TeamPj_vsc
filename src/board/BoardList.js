54988c1d293f97d4e588b15462a1b9cce8810bd1
betyani <dhwhdgns777@gmail.com>
2025-07-28 17:48:25 +0900

    리뷰게시판 리스트 페이징 처리 완료

--- src/board/BoardList.js
+++ src/board/BoardList.js
@@ -3,14 +3,24 @@ import axios from "axios";
 
 export default function BoardList({ productId }) {
 
-    const [review, setReivew] = useState([]);
+    const [review, setReview] = useState([]);
+    const [page, setPage] = useState(1);
+    const [pageInfo, setPageInfo] = useState({});
 
     useEffect(() => {
         const search = async () => {
             try {
                 console.log("상품id: ", productId);
-                const response = await axios.get("http://localhost:8080/cal/board/list", {params: {productId}});
-                setReivew(response.data);
+                const response = await axios.get("http://localhost:8080/cal/board/list",
+                    {
+                        params:
+                        {
+                            productId: productId,
+                            page: page
+                        }
+                    });
+                setReview(response.data.reviews);
+                setPageInfo(response.data.pageInfo);
                 console.log("불러온 리뷰: ", response.data);
             } catch (error) {
                 console.log(error);
@@ -19,7 +29,17 @@ export default function BoardList({ productId }) {
 
         search();
 
+    }, [productId, page]);
+
+
+    useEffect(() => {
+        setPage(1); // 상품이 바뀌면 페이지를 1로 초기화
     }, [productId]);
+
+
+    const goToPage = (pageNum) => {
+        setPage(pageNum);
+    }
 
     return (
         <>
@@ -33,6 +53,29 @@ export default function BoardList({ productId }) {
                     <hr />
                 </div>
             ))}
+
+            <div>
+                {pageInfo.hasPrev && (
+                    <button onClick={() => goToPage(pageInfo.startPage - 1)}>
+                        ◀ 이전
+                    </button>
+                )}
+
+                {Array.from({ length: pageInfo.endPage - pageInfo.startPage + 1 }, (_, index) => {
+                    const pageNum = pageInfo.startPage + index;
+                    return (
+                        <button key={ pageNum } onClick={() => goToPage(pageNum)} >
+                            [{ pageNum }]
+                        </button>);
+                })}
+
+                {pageInfo.hasNext && (
+                    <button onClick={() => goToPage(pageInfo.endPage + 1)}>
+                        다음▶
+                    </button>
+                )}
+
+            </div>
         </>
     );
 
